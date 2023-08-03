9a4883e74934a9d73164d7458e22bfe013feeca6
MuhXd <100521306+MuhXd@users.noreply.github.com>
2023-08-03 11:16:33 +0100

    Create loadstring.lua

---
+++ MoonClient/MoonClientLoaderstuff/Uiloader/loadstring.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://github.com/MuhXd/Full-Moon-Client/blob/main/MoonClient/MoonClientLoaderstuff/Uiloader/MainUi.lua?raw=true"))() 
+loadstring(game:HttpGet("https://github.com/MuhXd/Full-Moon-Client/blob/main/MoonClient/MoonClientLoaderstuff/Uiloader/FullMoonClient.gui?raw=true"))() 
+loadstring(game:HttpGet("https://github.com/MuhXd/Full-Moon-Client/blob/main/MoonClient/MoonClientLoaderstuff/Uiloader/SettingShield.lua?raw=true"))() 
