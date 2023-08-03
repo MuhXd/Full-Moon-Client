7d8000c1c8c60c52896f97258ff0f697f9e130dc
MuhXd <100521306+MuhXd@users.noreply.github.com>
2023-08-03 14:40:50 +0100

    Update Custom Mouse.lua

--- MoonClient/All/Custom Mouse.lua
+++ MoonClient/All/Custom Mouse.lua
@@ -1,10 +1,7 @@
 local GuiLib = loadstring(game:HttpGet("https://github.com/MuhXd/Full-Moon-Client/blob/main/MoonClient/MoonClientLoaderstuff/Template.lua?raw=true"))() 
 GuiLib.createButton({["Default"]=true,	["Name"]="Custom Mouse",["Icon"]=0},function(Value)
 	A = Value
-		if Value==false then
-			local m = player:GetMouse()
-m.Icon = "rbxasset://textures/Cursors/KeyboardMouse/ArrowFarCursor.png"
-		end
+	
 end,function(v)
 	print("settings")
 end)
@@ -16,13 +13,15 @@ local player = players.LocalPlayer
 
 while task.wait() do
 		local m = player:GetMouse()
-	if m.Icon == "http://www.roblox.com/asset/?id=569021388" then
-m.Icon = "rbxasset://textures/Cursors/KeyboardMouse/ArrowFarCursor.png"
-	end
+
 		if A then
 			m.Icon = "http://www.roblox.com/asset/?id=569021388"
 		
-		end
+	else
+	if m.Icon == "http://www.roblox.com/asset/?id=569021388" then
+m.Icon = "rbxasset://textures/Cursors/KeyboardMouse/ArrowFarCursor.png"
+	end
+	end
 
 	
 end
