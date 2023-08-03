80306433fffca7f70d5314b6b92e97dfc83f8353
MuhXd <100521306+MuhXd@users.noreply.github.com>
2023-08-03 16:36:44 +0100

    Update Optifine.lua

--- MoonClient/All/Optifine.lua
+++ MoonClient/All/Optifine.lua
@@ -3,9 +3,9 @@ local GuiLib = loadstring(game:HttpGet("https://github.com/MuhXd/Full-Moon-Clien
 GuiLib.createButton({["Default"]=true,	["Name"]="Optifine Zoom",["Icon"]=0},function(Value)
 	A = Value
     if Value == false then
-	game.Camera.FieldOfView=og
+	game.Workspace.Camera.FieldOfView=og
     end
-		og=game.Camera.FieldOfView
+		og=game.Workspace.Camera.FieldOfView
 end,function(v)
 	print("settings")
 end)
@@ -13,12 +13,12 @@ og=90
 
 game:GetService('UserInputService').InputBegan:Connect(function(inputObject, gameProcessed)
 	if inputObject.KeyCode == Enum.KeyCode.Z and A == true then
-og=game.Camera.FieldOfView	
-game.Camera.FieldOfView=30
+og=game.Workspace.Camera.FieldOfView	
+game.Workspace.Camera.FieldOfView=30
 	end
 end)
 game:GetService('UserInputService').InputEnded:Connect(function(inputObject, gameProcessed)
 	if inputObject.KeyCode == Enum.KeyCode.Z and A == true then
-		game.Camera.FieldOfView=og
+		game.Workspace.Camera.FieldOfView=og
 	end
 end)
