cd397b1745e2eaa53ca676ef4a29099f4a10088b
MuhXd <100521306+MuhXd@users.noreply.github.com>
2023-08-21 23:29:20 +0100

    Create Optifine.lua

---
+++ MoonClient/2059834811/Optifine.lua
@@ -0,0 +1,30 @@
+--optifine zoom 
+local GuiLib = loadstring(game:HttpGet("https://github.com/MuhXd/Full-Moon-Client/blob/main/MoonClient/MoonClientLoaderstuff/Template.lua?raw=true"))() 
+GuiLib.createButton({["Default"]=true,	["Name"]="Optifine Zoom",["Icon"]=0},function(Value)
+	AA = Value
+    game.ReplicatedStorage:WaitForChild("MoonClientCustomizion"):WaitForChild("Optifne").Value = Value
+    if Value == false then
+	game.Workspace.Camera.FieldOfView=og
+    end
+		og=game.Workspace.Camera.FieldOfView
+end,function(v)
+	print("settings")
+end)
+og=90
+
+game:GetService('UserInputService').InputBegan:Connect(function(inputObject, gameProcessed)
+	if inputObject.KeyCode == Enum.KeyCode.Z and AA == true then
+og=game.Workspace.Camera.FieldOfView
+			AAW=false
+			repeat 
+game.Workspace.Camera.FieldOfView=30
+				task.wait()
+			until AA == false or AAW == true
+	end
+end)
+game:GetService('UserInputService').InputEnded:Connect(function(inputObject, gameProcessed)
+	if inputObject.KeyCode == Enum.KeyCode.Z and AA == true then
+		game.Workspace.Camera.FieldOfView=og
+			AAW=true
+	end
+end)
