36e4102d18ac9a9cb2f28ee6efab8c1811beb2cf
MuhXd <100521306+MuhXd@users.noreply.github.com>
2023-08-24 19:17:52 +0100

    Update Optifine.lua

--- MoonClient/2059834811/Optifine.lua
+++ MoonClient/2059834811/Optifine.lua
@@ -1,7 +1,9 @@
 --optifine zoom 
 local SettingUiLib = loadstring(game:HttpGet("https://github.com/MuhXd/Full-Moon-Client/blob/main/MoonClient/MoonClientLoaderstuff/UiTemp.lua?raw=true"))() 
 local GuiLib = loadstring(game:HttpGet("https://github.com/MuhXd/Full-Moon-Client/blob/main/MoonClient/MoonClientLoaderstuff/Template.lua?raw=true"))() 
-GuiLib.createButton({["Default"]=true,	["Name"]="Optifine Zoom",["Icon"]=0},function(Value)
+Local Uisettinga = SettingUiLib.CreateUI("Optifine")
+
+GuiLib.createButton({["Default"]=true,	["Name"]="Optifine Zoom",["Icon"]=0,["HasOptionUi"]=true,["ui"]=Uisettinga},function(Value)
 	AA = Value
     game.ReplicatedStorage:WaitForChild("MoonClientCustomizion"):WaitForChild("Optifne").Value = Value
     if Value == false then
@@ -12,9 +14,13 @@ end,function(v)
 	print("settings")
 end)
 og=90
-
+Uisettinga.createButton(Uisettinga,{["Mode"]="Keybind",["Name"]="Keybinds",["Default"]=Enum.KeyCode.Z},function(Value)
+		KeycodeOpt = Value
+	end,function(v)
+	print("settings")
+end)
 game:GetService('UserInputService').InputBegan:Connect(function(inputObject, gameProcessed)
-	if inputObject.KeyCode == Enum.KeyCode.Z and AA == true then
+	if inputObject.KeyCode == KeycodeOpt and AA == true then
 og=game.Workspace.Camera.FieldOfView
 			AAW=false
 			repeat 
@@ -24,7 +30,7 @@ game.Workspace.Camera.FieldOfView=30
 	end
 end)
 game:GetService('UserInputService').InputEnded:Connect(function(inputObject, gameProcessed)
-	if inputObject.KeyCode == Enum.KeyCode.Z and AA == true then
+	if inputObject.KeyCode == KeycodeOpt and AA == true then
 		game.Workspace.Camera.FieldOfView=og
 			AAW=true
 	end
