d608e2b01ed0d5af6aa4a21bee84479e814e4074
MuhXd <100521306+MuhXd@users.noreply.github.com>
2023-08-24 20:36:02 +0100

    Update SprinttoggleV2.lua

--- MoonClient/2619619496/SprinttoggleV2.lua
+++ MoonClient/2619619496/SprinttoggleV2.lua
@@ -16,15 +16,35 @@ repeat
 until KnitGotten
 repeat task.wait() until debug.getupvalue(KnitClient.Start, 1)
 
+
 bedwars = {
 	ClientStoreHandler = require(game.Players.LocalPlayer.PlayerScripts.TS.ui.store).ClientStore,
 	FovController = KnitClient.Controllers.FovController,
 	SprintController = KnitClient.Controllers.SprintController
 }
+
+
 Sprint=false
 game:GetService('UserInputService').InputBegan:Connect(function(inputObject, gameProcessed)
 	if inputObject.KeyCode == Enum.KeyCode.LeftShift or inputObject.KeyCode == Enum.KeyCode.RightShift then
 		if Enabledsprint then
+local KnitGotten, KnitClient
+repeat
+	KnitGotten, KnitClient = pcall(function()
+		return debug.getupvalue(require(game.Players.LocalPlayer.PlayerScripts.TS.knit).setup, 6)
+	end)
+	if KnitGotten then break end
+	task.wait()
+until KnitGotten
+repeat task.wait() until debug.getupvalue(KnitClient.Start, 1)
+
+
+bedwars = {
+	ClientStoreHandler = require(game.Players.LocalPlayer.PlayerScripts.TS.ui.store).ClientStore,
+	FovController = KnitClient.Controllers.FovController,
+	SprintController = KnitClient.Controllers.SprintController
+}
+
 			Sprint=not Sprint
 			if Sprint then
 				repeat
