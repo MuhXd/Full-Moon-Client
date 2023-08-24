a276cb2a3b4dd78cb4b210dccc027c509ad924c1
MuhXd <100521306+MuhXd@users.noreply.github.com>
2023-08-24 20:29:22 +0100

    Update Custom Mouse v2.lua

--- MoonClient/2619619496/Custom Mouse v2.lua
+++ MoonClient/2619619496/Custom Mouse v2.lua
@@ -1,6 +1,6 @@
 local GuiLib = loadstring(game:HttpGet("https://github.com/MuhXd/Full-Moon-Client/blob/main/MoonClient/MoonClientLoaderstuff/Template.lua?raw=true"))() 
 GuiLib.createButton({["Default"]=true,	["Name"]="Custom Mouse",["Icon"]=0},function(Value)
-	A = Value
+	A2S = Value
 end,function(v)
 	print("settings")
 end)
@@ -11,9 +11,9 @@ local players = game:GetService("Players")
 local player = players.LocalPlayer
 -- bedwars mouse rbxassetid://8099700275
 while task.wait() do
-	if A and player:GetMouse().Icon == "rbxassetid://8099700275" or player:GetMouse().Icon == "http://www.roblox.com/asset/?id=569021388" and A==false then
+	if A2S and player:GetMouse().Icon == "rbxassetid://8099700275" or player:GetMouse().Icon == "http://www.roblox.com/asset/?id=569021388" and A2S==false then
 		local m = player:GetMouse()
-		if A then
+		if A2S then
 			m.Icon = "http://www.roblox.com/asset/?id=569021388"
 		else
 			m.Icon = "rbxassetid://8099700275"
