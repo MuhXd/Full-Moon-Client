cfe0e548cf58156e32335f9b2b1cc014c1ed44d6
MuhXd <100521306+MuhXd@users.noreply.github.com>
2023-08-24 22:29:08 +0100

    Update Optifine.lua

--- MoonClient/2619619496/Optifine.lua
+++ MoonClient/2619619496/Optifine.lua
@@ -33,11 +33,39 @@ bedwars = {
 }
 game:GetService('UserInputService').InputBegan:Connect(function(inputObject, gameProcessed)
 	if inputObject.KeyCode == KeycodeOpt and A == true then
+			local KnitGotten, KnitClient
+repeat
+	KnitGotten, KnitClient = pcall(function()
+		return debug.getupvalue(require(game.Players.LocalPlayer.PlayerScripts.TS.knit).setup, 6)
+	end)
+	if KnitGotten then break end
+	task.wait()
+until KnitGotten
+repeat task.wait() until debug.getupvalue(KnitClient.Start, 1)
+
+bedwars = {
+	ClientStoreHandler = require(game:GetService("Players").LocalPlayer.PlayerScripts.TS.ui.store).ClientStore,
+	FovController = KnitClient.Controllers.FovController
+}
 	bedwars.FovController:setFOV(30)
 	end
 end)
 game:GetService('UserInputService').InputEnded:Connect(function(inputObject, gameProcessed)
 	if inputObject.KeyCode == KeycodeOpt and A == true then
+			local KnitGotten, KnitClient
+repeat
+	KnitGotten, KnitClient = pcall(function()
+		return debug.getupvalue(require(game.Players.LocalPlayer.PlayerScripts.TS.knit).setup, 6)
+	end)
+	if KnitGotten then break end
+	task.wait()
+until KnitGotten
+repeat task.wait() until debug.getupvalue(KnitClient.Start, 1)
+
+bedwars = {
+	ClientStoreHandler = require(game:GetService("Players").LocalPlayer.PlayerScripts.TS.ui.store).ClientStore,
+	FovController = KnitClient.Controllers.FovController
+}
 		bedwars.FovController:setFOV(bedwars.ClientStoreHandler:getState().Settings.fov)
 	end
 end)
