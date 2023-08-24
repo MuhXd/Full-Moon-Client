401fd2e66ad0abf82276d95269743d57364c87e7
MuhXd <100521306+MuhXd@users.noreply.github.com>
2023-08-24 20:11:11 +0100

    Update Optifine.lua

--- MoonClient/All/Optifine.lua
+++ MoonClient/All/Optifine.lua
@@ -1,7 +1,11 @@
 --optifine zoom 
+local SettingUiLib = loadstring(game:HttpGet("https://github.com/MuhXd/Full-Moon-Client/blob/main/MoonClient/MoonClientLoaderstuff/UiTemp.lua?raw=true"))() 
 local GuiLib = loadstring(game:HttpGet("https://github.com/MuhXd/Full-Moon-Client/blob/main/MoonClient/MoonClientLoaderstuff/Template.lua?raw=true"))() 
-GuiLib.createButton({["Default"]=true,	["Name"]="Optifine Zoom",["Icon"]=0},function(Value)
+ Uisettinga = SettingUiLib.CreateUI("Optifine")
+local KeycodeOpt = Enum.KeyCode.Z
+GuiLib.createButton({["Default"]=true,	["Name"]="Optifine Zoom",["Icon"]=0,["HasOptionUi"]=true,["ui"]=Uisettinga},function(Value)
 	AA = Value
+    game.ReplicatedStorage:WaitForChild("MoonClientCustomizion"):WaitForChild("Optifne").Value = Value
     if Value == false then
 	game.Workspace.Camera.FieldOfView=og
     end
@@ -10,9 +14,14 @@ end,function(v)
 	print("settings")
 end)
 og=90
+SettingUiLib.createButton(Uisettinga,{["Mode"]="Keybind",["Name"]="Keybinds",["Default"]=Enum.KeyCode.Z},function(Value)
+		KeycodeOpt = Value
+	end,function(v)
+	print("settings")
+end)
 
 game:GetService('UserInputService').InputBegan:Connect(function(inputObject, gameProcessed)
-	if inputObject.KeyCode == Enum.KeyCode.Z and AA == true then
+	if inputObject.KeyCode == KeycodeOpt and AA == true then
 og=game.Workspace.Camera.FieldOfView
 			AAW=false
 			repeat 
@@ -21,8 +30,8 @@ game.Workspace.Camera.FieldOfView=30
 			until AA == false or AAW == true
 	end
 end)
-game:GetService('UserInputService').InputEnded:Connect(function(inputObject, gameProcessed)
-	if inputObject.KeyCode == Enum.KeyCode.Z and AA == true then
+game:GetService('UserInputService').InputEnded:Connect(function(inputObject, gameProcessed)	
+if inputObject.KeyCode == KeycodeOpt and AA == true then
 		game.Workspace.Camera.FieldOfView=og
 			AAW=true
 	end
